39f777585cfabf23b879c8a2c4d553a1c617b8bf
Phine1 <83701551+Phine1@users.noreply.github.com>
2022-03-28 11:14:48 +0000

    setup project structure

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'dark': '#0b0d17',
+      'lightblue': '#d0d6f9',
+      'white': '#ffffff',
+    },
+    fontFamily: {
+      'bel': 'Bellefair, serif',
+       'barC': 'Barlow Condensed, sans-serif',
+       'bar': 'Barlow, sans-serif',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
