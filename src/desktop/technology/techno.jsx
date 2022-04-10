b3c311b002d52290ce9ee99cae83d96941bb95b3
Phinehas Sersah <83701551+PhinehasSersah@users.noreply.github.com>
2022-04-10 00:36:40 +0000

    styled technology page

--- src/desktop/technology/techno.jsx
+++ src/desktop/technology/techno.jsx
@@ -14,8 +14,8 @@ const Technology = () => {
 
   const fetchData = async () => {
     await fetch("data.json").then((response) =>
-      response.json().then((data) => {
-        setData(data);
+      response.json().then((info) => {
+        setData(info);
         setIsFetched(true);
       })
     );
@@ -25,13 +25,26 @@ const Technology = () => {
     fetchData();
   }, [isFetched]);
 
-  // function to get index of moon, mars, europa, titan
+  // function to get index of space objects
   const changeRenderId = (event) => {
-    const target = event.target;
-    const parent = target.parentNode;
-    const index = Array.prototype.indexOf.call(parent.children, target);
-    setRenderId(index);
+    let target = event.target;
+    let renderIndex = target.textContent - 1;
+    setRenderId(renderIndex);
+    let parent = target.parentNode;
+    let gParent = parent.parentNode;
+    parent.style.backgroundColor = "white";
+    target.style.color = "black";
+    let parentSiblings = [...gParent.children].filter(
+      (child) => child !== parent
+    );
+    parentSiblings.forEach((child) => {
+      return (child.style.backgroundColor = "black");
+    });
+    parentSiblings.forEach((child) => {
+      return (child.children[0].style.color = "white");
+    });
   };
+
   return (
     <>
       <div
@@ -46,9 +59,9 @@ const Technology = () => {
             101
           </h1>
           <div className="flex  flex-row-reverse justify-between relative ">
-            <div className="flex flex-col h-5/6 w-1/3">
+            <div className="flex flex-col h-5/6 w-2/5">
               <img
-                className="w-4/5 transition-opacity mt-24 block mx-auto"
+                className="w-4/5 transition-opacity mt-2 block mx-auto"
                 src={images[renderId]}
                 alt="crew pictures"
               />
@@ -56,34 +69,40 @@ const Technology = () => {
 
             <div className="w-5/12 mt-36 ml-56 flex flex-row-reverse ">
               <div className="ml-32 w-full">
-                <h2 className="font-bel text-4xl text-faded mt- text-left tracking-widest uppercase ">
-                  {isFetched ? data.crew[renderId].role : null}
+                <h2 className="font-bar text-sm text-white text-left tracking-widest uppercase ">
+                  the terminology...
                 </h2>
                 <h1 className="font-bel text-5xl text-white mt-6 text-left tracking-widest uppercase mx-auto">
-                  {isFetched ? data.crew[renderId].name : null}
+                  {isFetched ? data.technology[renderId].name : null}
                 </h1>
-                <p className="text-white mt-12 w-96 h-48 font-bar text-left text-xl">
-                  {isFetched ? data.crew[renderId].bio : null}
+                <p className="text-white mt-8 w-96 h-48 font-bar text-left text-xl">
+                  {isFetched ? data.technology[renderId].description : null}
                 </p>
               </div>
               <div className="h-80 w-36 flex flex-col justify-evenly">
-                <div
-                  onClick={changeRenderId}
-                  className="w-16 h-16 border-2 border-white rounded-full cursor-pointer"
-                >
-                  <h1 className="text-2xl mt-3 text-white">1</h1>
+                <div className="w-16 h-16 bg-white border-2 border-faded rounded-full cursor-pointer">
+                  <h1
+                    onClick={changeRenderId}
+                    className="text-2xl w-full font-bel h-full pt-3 text-dark"
+                  >
+                    1
+                  </h1>
                 </div>
-                <div
-                  onClick={changeRenderId}
-                  className="w-16 h-16 border-2 border-white rounded-full cursor-pointer"
-                >
-                  <h1 className="text-2xl mt-3 text-white">2</h1>
+                <div className="w-16 h-16 border-2  border-faded rounded-full cursor-pointer">
+                  <h1
+                    onClick={changeRenderId}
+                    className="text-2xl w-full font-bel h-full pt-3 text-white"
+                  >
+                    2
+                  </h1>
                 </div>
-                <div
-                  onClick={changeRenderId}
-                  className="w-16 h-16 border-2 border-white rounded-full cursor-pointer"
-                >
-                  <h1 className="text-2xl mt-3 text-white">3</h1>
+                <div className="w-16 h-16 border-2 border-faded rounded-full cursor-pointer">
+                  <h1
+                    onClick={changeRenderId}
+                    className="text-2xl w-full font-bel h-full pt-3  text-white"
+                  >
+                    3
+                  </h1>
                 </div>
               </div>
             </div>
