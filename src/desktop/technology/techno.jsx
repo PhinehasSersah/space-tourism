1ca3a76c5c9378f619583d52af27406cdbc3e4b7
Phinehas Sersah <83701551+PhinehasSersah@users.noreply.github.com>
2022-04-22 14:05:07 +0000

    styled mobile technology page

--- src/desktop/technology/techno.jsx
+++ src/desktop/technology/techno.jsx
@@ -61,7 +61,7 @@ const Technology = () => {
           <div className="flex flex-col lg:flex-row-reverse lg:justify-between lg:relative ">
             <div className="flex mx-auto mt-12 lg:flex-col w-screen h-60 lg:h-5/6 lg:w-2/5 ">
               <img
-                className="lg:w-4/5 w-full  object-cover block mx-auto"
+                className="lg:w-4/5 w  object-cover block mx-auto"
                 src={images[renderId]}
                 alt="crew pictures"
               />
@@ -70,20 +70,24 @@ const Technology = () => {
 
 
 
-            <div className="w-5/12 mt-36 ml-56 flex flex-col-reverse lg:flex-row-reverse ">
-              <div className="ml-32 w-full">
-                <h2 className="font-bar text-sm text-white text-left tracking-widest uppercase ">
+            <div className="mt-36 lg:ml-56 flex flex-col-reverse lg:flex-row-reverse  ">
+
+              <div className=" mt-8 lg:ml-32 w-96 mx-auto">
+                <h2 className="font-barC text-sm text-white text-center lg:text-left tracking-widest uppercase ">
                   the terminology...
                 </h2>
-                <h1 className="font-bel text-5xl text-white mt-6 text-left tracking-widest uppercase mx-auto">
+                <h1 className="font-bel text-2xl lg:text-5xl text-white mt-6 lg:text-left tracking-widest uppercase mx-auto">
                   {isFetched ? data.technology[renderId].name : null}
                 </h1>
-                <p className="text-white mt-8 w-96 h-48 font-bar text-left text-xl">
+                <p className="text-white mt-8 w-96 h-48 font-bar lg:text-left text-base lg:text-xl">
                   {isFetched ? data.technology[renderId].description : null}
                 </p>
               </div>
-              <div className="lg:h-80 lg:w-36 -mt-12 flex lg:flex-col justify-evenly">
-                <div className="w-16 h-16 bg-white border-2 border-faded rounded-full cursor-pointer">
+
+
+
+              <div className="lg:h-80 lg:w-36 mx-auto -mt-12 flex lg:flex-col justify-evenly">
+                <div className="w-16 h-16 bg-white border-2 mx-2 border-faded rounded-full cursor-pointer">
                   <h1
                     onClick={changeRenderId}
                     className="text-2xl w-full font-bel h-full pt-3 text-dark"
@@ -91,7 +95,7 @@ const Technology = () => {
                     1
                   </h1>
                 </div>
-                <div className="w-16 h-16 border-2  border-faded rounded-full cursor-pointer">
+                <div className="w-16 h-16 border-2  border-faded mx-2 rounded-full cursor-pointer">
                   <h1
                     onClick={changeRenderId}
                     className="text-2xl w-full font-bel h-full pt-3 text-white"
@@ -99,7 +103,7 @@ const Technology = () => {
                     2
                   </h1>
                 </div>
-                <div className="w-16 h-16 border-2 border-faded rounded-full cursor-pointer">
+                <div className="w-16 h-16 border-2 border-faded mx-2 rounded-full cursor-pointer">
                   <h1
                     onClick={changeRenderId}
                     className="text-2xl w-full font-bel h-full pt-3  text-white"
